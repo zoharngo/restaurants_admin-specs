00cb3fd9d73ed90290c6d3874103d251d9153656
zoharngo <zoharngo@gmail.com>
2018-10-24 11:12:11 +0300

    modify tests

--- test/tests.js
+++ test/tests.js
@@ -9,7 +9,7 @@ const chai = require('chai'),
     chaiAsPromised = require('chai-as-promised');
 
 chai.use(chaiAsPromised);
-const url = process.env.URL || 'http://localhost:8000/todos';
+const url = process.env.URL || 'http://localhost:8000/api';
 
 describe('Cross Origin Requests', () => {
     let result;
@@ -33,69 +33,64 @@ describe('Cross Origin Requests', () => {
     });
 });
 
-describe('Create Todo Item', () => {
+describe('Create Restaurant', () => {
     let result;
+    let item;
 
     before(() => {
-        result = post(url, { title: 'Walk the dog' });
+        result = post(url, [{'restaurant_name' : 'Hudson'}]);
     });
 
     it('should return a 201 CREATED response', () => {
         return assert(result, 'status').to.equal(201);
     });
 
-    it('should recevie a location hyperlink', () => {
-        return assert(result, 'header').to.contain.
-            property('location').match(/^https?:\/\/.+\/todos\/[\d]+$/);
-    });
-
-
-    it("should have title property equal to 'Walk the dog'", () => {
-        let item = result.then((res) => {
-            return get(res.header['location']);
+    it("should have restaurant_name property equal to 'Hudson'", () => {
+        item = result.then((res) => {;
+            return get(url.concat('/').concat(res['body'][0].uuid));
         });
-        return assert(item, 'body').to.contain.property('title').equal('Walk the dog');
+        return assert(item, 'body').to.contain.property('restaurant_name').equal('Hudson');
     });
 
     after(() => {
         return del(url);
     });
 });
-describe('Update Todo Item', () => {
+describe('Update Restaurant', () => {
     let location;
 
     beforeEach((done) => {
-        post(url, { title: 'Walk the dog' }).then((res) => {
-            location = res.header['location'];
+        post(url, [{ title: 'Hudson' }]).then((res) => {
+            location = url.concat('/').concat(res['body'][0].uuid);
             done();
         });
     });
 
 
-    it('should have completed set to true after PUT update', () => {
+    it('should have restaurant_type set to grill after PUT update', () => {
         let result = update(location, 'PUT',
-            { completed: true });
-        return assert(result, 'body').to.contain.property('completed').equal(true);
+            { 'restaurant_type': 'grill' });
+        return assert(result, 'body').to.contain.property('restaurant_type').equal('grill');
     });
 
 
-    it('should have completed set to true after PATCH update', () => {
+    it('should have phone set to (+972) 050 - 4945555 PATCH update', () => {
         let result = update(location, 'PATCH',
-            { 'completed': true });
-        return assert(result, 'body').to.contain.property('completed').equal(true);
+            { 'phone': '(+972) 050 - 4945555' });
+        return assert(result, 'body').to.contain.property('phone').equal('(+972) 050 - 4945555');
     });
 
     after(() => {
         return del(url);
     });
 });
-describe('Delete Todo Item', () => {
+describe('Delete Restaurant', () => {
     let location;
 
     beforeEach((done) => {
-        post(url, { title: 'Walk the dog' })
+        post(url, [{ title: 'BBB' }])
             .then((res) => {
-                location = res.header['location'];
+                location = url.concat('/').concat(res['body'][0].uuid);
                 done();
             });
     });
@@ -105,7 +100,7 @@ describe('Delete Todo Item', () => {
         return assert(result, 'status').to.equal(204);
     });
 
-    it('should delete the item', () => {
+    it('should delete the restaurant', () => {
         let result = del(location).then((res) => {
             return get(location);
         });
